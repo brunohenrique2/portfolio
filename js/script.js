c34a3f6d2bd175a29cd2f669be05ffcdf51f7524
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2023-07-01 13:08:29 -0300

    refazendo header

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
-function clickMenu() {
-    var btn_burger = document.getElementById('btn-hamburger')
-    var menu = document.getElementById('mobile-menu');
+//Variaveis
+const btn_mobile = document.getElementById('btn-mobile')
 
-    if (menu.style.display == 'block') {
-        menu.style.display = 'none'
-        btn_burger.innerText = 'menu'
-    }else {
-        menu.style.display = 'block'
-        btn_burger.innerText = 'close'
-    }
+//Eventos
+btn_mobile.addEventListener('click', toggleMenu)
 
+//Funções
+function toggleMenu() {
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active')
 }
