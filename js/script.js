274c5bcafc933416db0aa6818d11c0d58139a698
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2023-07-10 11:22:08 -0300

    sessão projetos atualizada

--- js/script.js
+++ js/script.js
@@ -7,5 +7,5 @@ btn_mobile.addEventListener('click', toggleMenu)
 //Funções
 function toggleMenu() {
     const nav = document.getElementById('nav')
-    nav.classList.toggle('active')
+    nav.classList.toggle('active')  
 }
