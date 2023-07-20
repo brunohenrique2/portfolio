4d35121d4ce5b6278ff2d48d36dbe44f719a4254
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2023-07-20 15:55:10 -0300

    new desighn v0.3.1

---
+++ js/loading.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+    const loader = document.querySelector('div.loader')
+
+    loader.classList.add("loader-hidden")
+
+    loader.addEventListener('transitionend', () => {
+        document.body.removeChild("loader")
+    })
+})
