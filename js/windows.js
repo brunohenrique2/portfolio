474e3801a62c8f579f07de999accf4e8e53f4aad
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2023-08-04 17:41:16 -0300

    correções

--- js/windows.js
+++ js/windows.js
@@ -6,17 +6,15 @@ const btnClose = document.querySelectorAll('span.screenclose')
 for(let i=0; i<btnClose.length; i++) {
     btnClose[i].addEventListener('click', () => {
             screen[i].style.display = 'none'
-        console.log(i)
     })
 }
 
 for(let i=0; i<navLinks.length; i++) {
     navLinks[i].addEventListener('click', () => {
-            if(screen[i].style.display == 'block') {
-                screen[i].style.display = 'none'
-            }else if(screen[i].style.display == 'none') {
-                screen[i].style.display = 'block'
-            }
-        console.log(i)
+        if(screen[i].style.display == 'block') {
+            screen[i].style.display = 'none'
+        }else if(screen[i].style.display == 'none') {
+            screen[i].style.display = 'block'
+        }
     })
 }
